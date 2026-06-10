94a0963a96ae2d11e280c1cc0e4386cbc77ee9a0
amallic8 <60700605+amallic8@users.noreply.github.com>
2026-06-10 15:26:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("Bike Business Record - Sheet1.csv")
+
+df["total_profit"] = pd.to_numeric(df["total_profit"], errors="coerce")
+
+st.title("Bike Business Dashboard")
+
+st.metric("Total Profit", f"₹{df['total_profit'].sum():,.0f}")
+
+brand_profit = (
+    df.groupby("bike_brand")["total_profit"]
+    .sum()
+    .reset_index()
+)
+
+fig = px.bar(
+    brand_profit,
+    x="bike_brand",
+    y="total_profit",
+    title="Profit by Brand"
+)
+
+st.plotly_chart(fig)
+
+st.dataframe(df)
